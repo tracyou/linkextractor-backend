170fa66a7af96c5a83f3e60622f1df06729814fc
pandemiek <mikarchristian@gmail.com>
2023-11-30 12:17:06 +0100

    :sparkles: implement migration files and models for Annotation, Matter and MatterRelation

---
+++ app/Models/Annotation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\Annotation
+ *
+ * @property string $id
+ * @property string $matter_id
+ * @property string $text
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read Matter $matter
+ * @method static Builder|Annotation newModelQuery()
+ * @method static Builder|Annotation newQuery()
+ * @method static Builder|Annotation onlyTrashed()
+ * @method static Builder|Annotation query()
+ * @method static Builder|Annotation whereCreatedAt($value)
+ * @method static Builder|Annotation whereDeletedAt($value)
+ * @method static Builder|Annotation whereId($value)
+ * @method static Builder|Annotation whereMatterId($value)
+ * @method static Builder|Annotation whereText($value)
+ * @method static Builder|Annotation whereUpdatedAt($value)
+ * @method static Builder|Annotation withTrashed()
+ * @method static Builder|Annotation withoutTrashed()
+ * @mixin Eloquent
+ */
+final class Annotation extends Model
+{
+    protected $table = 'annotations';
+
+    public function matter(): BelongsTo
+    {
+        return $this->belongsTo(Matter::class, 'matter_id', 'id');
+    }
+}
---
+++ database/migrations/2023_11_30_092538_create_annotations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('annotations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('matter_id');
+            $table->text("text");
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('matter_id')->on('matters')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('annotations');
+    }
+};
