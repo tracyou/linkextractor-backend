8e39e51dce04b13ac3c5fc4ad1d68ad4caf17135
pandemiek <mikarchristian@gmail.com>
2023-11-30 12:17:06 +0100

    :sparkles: implement migration files and models for Annotation, Matter and MatterRelation

---
+++ database/migrations/2023_11_30_084732_create_matters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matters', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->char('color', 7)->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matters');
+    }
+};
