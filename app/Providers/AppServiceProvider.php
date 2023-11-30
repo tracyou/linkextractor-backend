a3aa17d1ff268b2f5d42b0cf5fe92409dfadf256
pandemiek <mikarchristian@gmail.com>
2023-11-30 12:17:48 +0100

    :zap: add Annotation and MatterRelation to MorphMap in boot function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Annotation;
+use App\Models\Matter;
+use App\Models\MatterRelation;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -10,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
     {
     }
 
-    public function boot()
+    public function boot(): void
     {
+        Relation::enforceMorphMap([
+           'matter'=>Matter::class,
+            'annotation'=>Annotation::class,
+            'matterRelation'=>MatterRelation::class,
+        ]);
     }
 }
