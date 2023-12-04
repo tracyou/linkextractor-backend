c99815e52fb17be68c3d875fcca04a27c4d42c21
pandemiek <mikarchristian@gmail.com>
2023-12-04 13:39:41 +0100

    :white_check_mark: write small unofficial test to test factory and model relations.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Factories\AnnotationFactory;
+use App\Factories\MatterRelationFactory;
+use App\Http\Controllers\MatterRelationController;
 use App\Models\Annotation;
 use App\Models\Law;
+use App\Models\Matter;
+use App\Models\MatterRelation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +31,18 @@ Route::get('/test', function () {
 
     dd($annotation->law()->withPivot(['cursor_index'])->get());
 });
+
+Route::get('/annotation-test', function () {
+    $annotation = (new AnnotationFactory)->create(Matter::find('9ac49f50-61d9-4fdb-b1b9-28f2ef5ff970'), 'dit is een annotatie');
+    $retrieved = Annotation::find($annotation->id);
+    dd($retrieved->matter);
+});
+
+Route::get('/matter-relations-test', function () {
+    $matterA = Matter::create(['name' => 'Matter A', 'color' => '#000000']);
+    $matterB = Matter::create(['name' => 'Matter B', 'color' => '#ffffff']);
+    $relation = (new MatterRelationFactory)->create($matterA, $matterB, 'requires 1', 'Description');
+    $retrievedRelation = MatterRelation::find($relation->id);
+    dd($retrievedRelation->matterA, $retrievedRelation->matterB);
+});
+
