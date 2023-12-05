7e09f7841ad46a6ea2f8983626b97b58bf95f595
Alexander Vreeswijk <alexander.vreeswijk@hva.nl>
2023-12-05 22:18:50 +0100

    Feature: Add comment attribute to annotation_law table and fix snake_case naming

--- database/migrations/2023_11_30_142830_create_laws_annotations_table.php
+++ database/migrations/2023_11_30_142830_create_laws_annotations_table.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
+
     /** Run the migrations. */
     public function up(): void
     {
@@ -14,10 +15,11 @@ return new class () extends Migration {
             $table->foreignUuid('law_id')->constrained()->cascadeOnDelete();
             $table->foreignUuid('annotation_id')->constrained()->cascadeOnDelete();
             $table->integer('cursor_index');
+            $table->string('comment')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
-            $table->primary(['law_id','annotation_id']);
+            $table->unique(['law_id', 'annotation_id', 'cursor_index']);
         });
     }
 
