763df63f593b23b833ae27bb24c38a9ef901cc74
Mo <mo.kadib.alban@hva.nl>
2023-12-14 11:02:11 +0100

    feat: save annotated law in backend

--- app/GraphQL/Mutations/SaveAnnotatedLaw.php
+++ app/GraphQL/Mutations/SaveAnnotatedLaw.php
@@ -3,26 +3,18 @@
 namespace App\GraphQL\Mutations;
 
 use App\Contracts\Factories\AnnotationFactoryInterface;
-use App\Contracts\Repositories\AnnotationRepositoryInterface;
-use App\Contracts\Repositories\ArticleRepositoryInterface;
 use App\Contracts\Repositories\LawRepositoryInterface;
-use App\Contracts\Repositories\MatterRelationRepositoryInterface;
-use App\Contracts\Repositories\MatterRelationSchemaRepositoryInterface;
 use App\Contracts\Repositories\MatterRepositoryInterface;
 use App\Models\Law;
-
+use Database\Factories\AnnotationFactory;
 
 class SaveAnnotatedLaw
 {
 
     public function __construct(
-        protected LawRepositoryInterface                  $lawRepository,
-        protected AnnotationRepositoryInterface           $annotationRepository,
-        protected MatterRepositoryInterface               $matterRepository,
-        protected AnnotationFactoryInterface              $annotationFactory,
-        protected ArticleRepositoryInterface              $articleRepository,
-        protected MatterRelationRepositoryInterface       $matterRelationRepository,
-        protected MatterRelationSchemaRepositoryInterface $matterRelationSchemaRepository,
+        protected LawRepositoryInterface     $lawRepository,
+        protected AnnotationFactoryInterface $annotationFactory,
+        protected MatterRepositoryInterface  $matterRepository,
 
     )
     {
@@ -31,36 +23,24 @@ class SaveAnnotatedLaw
     public function __invoke($_, array $args): Law|null
     {
         $lawId = $args['lawId'];
-        $lawTitle = $args['title'];
         $isPublished = $args['isPublished'];
-        $articles = collect($args['articles']);
+        $annotations = collect($args['annotations']);
 
 
         /** @var Law $law */
         $law = $this->lawRepository->findOrFail($lawId);
 
         $law->is_published = $isPublished;
-        $law->title = $lawTitle;
 
-        $articles->each(function (array $articleInput) {
-            $article = $this->articleRepository->findOrFail($articleInput['articleId']);
-            $annotations = collect($articleInput['annotations'])->each(function ($annotationInput) use ($article) {
-                $matter = $this->matterRepository->findOrFail($annotationInput['matterId']);
-                $comment = $annotationInput['comment'];
-                $definition = $annotationInput['definition'];
-                $text = $annotationInput['text'];
-                $matterRelationSchema = $this->matterRelationSchemaRepository->findOrFail($annotationInput['matterRelationSchemaId']);
 
-                return $this->annotationFactory->create(
-                    $matter,
-                    $text,
-                    $definition,
-                    $comment,
-                    $article,
-                    $matterRelationSchema
-                );
-            });
+        $annotations->each(function (array $annotationInput) use ($law) {
+            $matter = $this->matterRepository->findOrFail($annotationInput['matterId']);
+            $annotation = $this->annotationFactory->create($matter, $annotationInput['text']);
 
+            $law->annotations()->attach($annotation->id, [
+                'cursor_index' => $annotationInput['cursorIndex'],
+                'comment' => $annotationInput['comment'],
+            ]);
         });
 
         $law->save();
