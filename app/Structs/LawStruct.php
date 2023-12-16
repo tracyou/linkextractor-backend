92397fd72794ed261019591018b2f432135cab78
Stefan de Haan <info@stefandehaan.com>
2023-12-16 14:31:07 +0100

    Feat: Create import for xml lawbooks

--- app/Structs/LawStruct.php
+++ app/Structs/LawStruct.php
@@ -4,23 +4,26 @@ namespace App\Structs;
 
 use App\Models\Law;
 
-final class LawBookStruct
+final class LawStruct
 {
     public function __construct(
-        public ?int $lawBookNumber = null,
-        public ?string $lawBookName = null,
         public ?string $label = null,
         public ?string $nr = null,
         public ?string $titel = null,
         public ?string $text = null,
-    ) { }
+    )
+    {
+    }
 
-    public function toModel(): Law
+    public function toModel(): ?Law
     {
         $law = new Law();
-        $law->title = $this->label . $this->nr . $this->titel;
+        $law->title = $this->label . ' ' . $this->nr . ' ' . $this->titel;
+        if (! $this->text) {
+            return null;
+        }
         $law->text = $this->text;
-        $law->isPublished = false;
+        $law->is_published = false;
 
         return $law;
     }
