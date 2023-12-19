d3fd4f759d5f2cb73c9cce29c16eb1a051cd0f91
Mo <mo.kadib.alban@hva.nl>
2023-12-19 12:54:12 +0100

    fix: fix merge confilcts and add some final touches

--- database/migrations/2023_12_19_100331_create_articles_table.php
+++ database/migrations/2023_12_19_100331_create_articles_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,12 +20,19 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        Schema::table('annotations', function (Blueprint $table) {
+
+            $table->foreignUuid('article_id')->constrained()->cascadeOnDelete();
+
+        });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public
+    function down(): void
     {
         Schema::dropIfExists('articles');
     }
