f731588c6ce5cca38979dfe3c28c24676fdae128
Mo <mo.kadib.alban@hva.nl>
2023-12-21 08:38:17 +0100

    test: test the matter has many annotations

--- app/Contracts/Factories/ArticleFactoryInterface.php
+++ app/Contracts/Factories/ArticleFactoryInterface.php
@@ -4,11 +4,10 @@ namespace App\Contracts\Factories;
 
 use App\Models\Article;
 use App\Models\Law;
-use PHPUnit\Util\Json;
-
+use Psy\Util\Json;
 
 interface ArticleFactoryInterface
 {
-    public function create(Law $law, string $title, string $text, array $jsonData): Article;
+    public function create(Law $law, string $title, string $text): Article;
 
 }
--- tests/Feature/MatterFactoryTest.php
+++ tests/Feature/MatterFactoryTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Feature;
 
-use App\Enums\MatterRelationEnum;
+use App\Contracts\Factories\AnnotationFactoryInterface;
+use App\Contracts\Factories\ArticleFactoryInterface;
+use App\Contracts\Factories\LawFactoryInterface;
+use App\Contracts\Factories\MatterFactoryInterface;
+use App\Contracts\Factories\MatterRelationSchemaFactoryInterface;
 use App\Factories\AnnotationFactory;
 use App\Factories\MatterFactory;
 use App\Factories\MatterRelationFactory;
-use App\Factories\MatterRelationSchemaFactory;
-use App\Factories\RelationSchemaFactory;
-use App\Models\Article;
-use App\Models\MatterRelationSchema;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -17,37 +17,62 @@ class MatterFactoryTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $annotationFactory;
+    protected $matterFactory;
+    protected $lawFactory;
+    protected $matterRelationSchemaFactory;
+    protected $articleFactory;
+    protected $matterRelation;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->annotationFactory = $this->app->make(AnnotationFactoryInterface::class);
+        $this->matterFactory = $this->app->make(MatterFactoryInterface::class);
+        $this->lawFactory = $this->app->make(LawFactoryInterface::class);
+        $this->matterRelationSchemaFactory = $this->app->make(MatterRelationSchemaFactoryInterface::class);
+        $this->articleFactory = $this->app->make(ArticleFactoryInterface::class);
+        $this->matterRelation = $this->app->make(MatterRelationFactory::class);
+    }
+
     public function testMatterHasManyAnnotations(): void
     {
-        $matter = (new MatterFactory())->create("matter", "#000000");
-        $article = Article::factory()->create();
-        $relationSchema = (new RelationSchemaFactory())->create(true);
-        (new AnnotationFactory())->create(
-            schema: $relationSchema,
-            article: $article,
-            matter: $matter,
-            text: "this is an annotation"
+        //Act
+        $matter = $this->matterFactory->create('matter', '#001000');
+        $law = $this->lawFactory->create('title', false);
+        $article = $this->articleFactory->create($law, 'title of the article', 'this is the text of the article');
+        $matterRelationSchema = $this->matterRelationSchemaFactory->create();
+        $annotation1 = $this->annotationFactory->create(
+            $matter,
+            'this is an annotation',
+            200,
+            'this is a comment',
+            $article,
+            $matterRelationSchema
         );
-        $this->assertEquals(1, $matter->annotations->count());
+        $annotation2 = $this->annotationFactory->create(
+            $matter,
+            'this is an annotation',
+            200,
+            'this is a comment',
+            $article,
+            $matterRelationSchema
+        );
+
+        // Assert
+        $this->assertEquals(2, $matter->annotations->count());
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $matter->annotations);
+
     }
 
     public function testMatterHasManyRelations(): void
     {
-        $matter = (new MatterFactory())->create("matter1", "#000000");
-        $relatedMatter = (new MatterFactory())->create("matter2", "#000000");
-        $relationSchema = (new RelationSchemaFactory())->create(true);
-        $matterRelationSchema = (new MatterRelationSchemaFactory())->create(
-            matter: $matter,
-            relationSchema: $relationSchema,
-            schemaLayout: "{}"
-        );
-        (new MatterRelationFactory())->create(
-            relatedMatter: $relatedMatter,
-            schema      : $matterRelationSchema,
-            relation    : MatterRelationEnum::REQUIRES_ONE(),
-            description : "description",
-        );
-        $this->assertEquals(1, $matter->matterRelations->count());
-        $this->assertInstanceOf(MatterRelationSchema::class, $matter->matterRelations->first()->matterRelationSchema);
+        $matterA = (new MatterFactory())->create("matter1", "#000000");
+        $matterB = (new MatterFactory())->create("matter2", "#000000");
+        (new MatterRelationFactory())->create($matterA, $matterB, "requires 1", "description");
+        $this->assertEquals(1, $matterA->matterRelationsParents->count());
+        $this->assertEquals(1, $matterB->matterRelationsChilds->count());
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $matterA->matterRelationsParents);
     }
 }
