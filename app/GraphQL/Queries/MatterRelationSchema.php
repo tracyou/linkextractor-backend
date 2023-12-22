cedac2bd328461c4700d3e115458d559eac06307
Alexander Vreeswijk <alexander.vreeswijk@hva.nl>
2023-12-22 18:38:35 +0100

    feat: write query for finding schemas by id

---
+++ app/GraphQL/Queries/MatterRelationSchema.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Contracts\Repositories\MatterRelationSchemaRepositoryInterface;
+
+final class MatterRelationSchema
+{
+    public function __construct(
+        protected MatterRelationSchemaRepositoryInterface $matterRelationSchemaRepository,
+    ) {
+    }
+
+    /** @param array<string, mixed> $args */
+    public function __invoke(null $_, array $args): \App\Models\MatterRelationSchema | null
+    {
+        $relationSchemaId = $args['relation_schema_id'];
+        $matterId = $args['matter_id'];
+
+        return $this->matterRelationSchemaRepository->getMatterRelationSchema($relationSchemaId, $matterId);
+    }
+}
