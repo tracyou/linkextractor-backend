2b6b0bd0a3e644cbe9248a988b19b06e363316c0
Alexander Vreeswijk <alexander.vreeswijk@hva.nl>
2023-12-23 01:51:26 +0100

    feat: write tests for new query

---
+++ tests/Http/GraphQL/Enums/MatterRelationEnum/Queries/MatterRelationTypesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Http\GraphQL\Enums\MatterRelationEnum\Queries;
+
+use App\Enums\MatterRelationEnum;
+use App\GraphQL\Queries\MatterRelationTypes;
+use Tests\Http\GraphQL\AbstractHttpGraphQLTestCase;
+
+class MatterRelationTypesTest extends AbstractHttpGraphQLTestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_matter_relation_types(): void
+    {
+        $outcome = [];
+        foreach (MatterRelationEnum::asArray() as $value) {
+            $outcome[] = [
+                'key' => MatterRelationEnum::fromValue($value)->key,
+                'value' => $value,
+            ];
+        }
+
+        $this->graphQL(/** @lang GraphQL */ '
+            query {
+                matterRelationTypes {
+                    key
+                    value
+                }
+            }
+        ')->assertJson([
+            'data' => [
+                'matterRelationTypes' => $outcome,
+            ],
+        ]);
+    }
+}
