2656eb7245104469630fae6a7984b323dce5e681
Alexander Vreeswijk <alexander.vreeswijk@hva.nl>
2023-12-23 01:51:15 +0100

    feat: create query for getting all matter relation enum entries

---
+++ app/GraphQL/Queries/MatterRelationTypes.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Enums\MatterRelationEnum;
+
+class MatterRelationTypes
+{
+    /**
+     * @param null                  $_
+     * @param array<string, string> $args
+     * @return array<int, array<string, mixed>>
+     */
+    public function __invoke($_, array $args): array
+    {
+        $statuses = [];
+
+        foreach (MatterRelationEnum::asArray() as $value) {
+            $statuses[] = [
+                'key' => $value,
+                'value' => $value,
+            ];
+        }
+
+        return $statuses;
+    }
+}
