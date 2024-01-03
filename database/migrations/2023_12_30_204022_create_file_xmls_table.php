b3af1494eeb38739076b9e361694835a06e11ee8
Mo <mo.kadib.alban@hva.nl>
2024-01-03 14:14:24 +0100

    in progress

--- database/migrations/2023_12_30_204022_create_file_xmls_table.php
+++ database/migrations/2023_12_30_204022_create_file_xmls_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('file_xmls', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('title');
             $table->text('content');
             $table->timestamps();
