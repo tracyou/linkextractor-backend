fd00e1bb140c1d03e065a53f33d0b2c92cb400bc
Stefan de Haan <info@stefandehaan.com>
2024-01-11 20:10:04 +0100

    Fix

---
+++ app/GraphQL/Queries/ArticlesByLaw.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Article;
+use App\Models\Annotation;
+use Illuminate\Support\Collection;
+use App\Contracts\Repositories\ArticleRepositoryInterface;
+
+class ArticlesByLaw
+{
+    public function __construct(
+        protected ArticleRepositoryInterface $articleRepository,
+    ) {
+    }
+
+    /**
+     * @param null                  $_
+     * @param array<string, mixed>  $args
+     *
+     * @return array<int, Article>
+     */
+    public function __invoke(null $_, array $args): array
+    {
+        $id = $args['law_id'];
+
+        return $this->articleRepository->where('law_id', $id)->toArray();
+    }
+}
