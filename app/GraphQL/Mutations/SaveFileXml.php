0053d8de2f77a4745631873544efd11d7c736454
mo <mo.kadib.alban@hva.nl>
2024-01-15 10:56:30 +0100

    test: test save annotated law mutation

--- app/GraphQL/Mutations/SaveFileXml.php
+++ app/GraphQL/Mutations/SaveFileXml.php
@@ -13,7 +13,6 @@ class SaveFileXml
 {
     public function __construct(
         protected FileXmlFactoryInterface    $fileXmlFactory,
-        protected FileXmlRepositoryInterface $fileXmlRepository
     )
     {
     }
--- tests/Http/GraphQL/Models/FileXml/Queries/FileXmlsTest.php
+++ tests/Http/GraphQL/Models/FileXml/Queries/FileXmlsTest.php
@@ -2,7 +2,94 @@
 
 namespace Tests\Http\GraphQL\Models\FileXml\Queries;
 
-class FileXmlsTest
+use Tests\Http\GraphQL\AbstractHttpGraphQLTestCase;
+
+class FileXmlsTest extends AbstractHttpGraphQLTestCase
 {
+    /**
+     * @test
+     */
+    public function it_saves_imported_file(): void
+    {
+        // Arrange //Act
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                saveFileXml(input: {
+                    title: "this is the file title",
+                    content: "this is the file content"
+                }) {
+                    title,
+                    content
+                }
+            }
+        ');
+
+        // Assert
+        $response->assertJson([
+            'data' => [
+                'saveFileXml' => [
+                    'title' => 'this is the file title',
+                    'content' => 'this is the file content'
+                ],
+            ],
+        ]);
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_all_imported_files()
+    {
+        // Arrange
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                saveFileXml(input: {
+                    title: "this is the file title",
+                    content: "this is the file content"
+                }) {
+                    title,
+                    content
+                }
+            }
+        ');
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                saveFileXml(input: {
+                    title: "this is the file 2 title",
+                    content: "this is the file 2 content"
+                }) {
+                    title,
+                    content
+                }
+            }
+        ');
+
+        // Act
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            query {
+                fileXmls{
+                    title
+                }
+            }
+        ');
+
+        // Assert
+        $response->assertJson([
+            'data' => [
+                'fileXmls' => [
+                    [
+                        'title' => 'this is the file title',
+
+                    ],[
+                        'title' => 'this is the file 2 title'
+                    ]
+                ]
+            ]
+        ]);
+
+    }
+
 
 }
