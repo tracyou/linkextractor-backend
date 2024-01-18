aee4f20bf603048b58772f046be83ff97a0ebd5e
pandemiek <mikarchristian@gmail.com>
2024-01-18 11:07:47 +0100

    fix: remove references to PancakeFactoryInterface and PancakeStackFactoryInterface in FactoryServiceProvider.php

--- app/Providers/FactoryServiceProvider.php
+++ app/Providers/FactoryServiceProvider.php
@@ -11,8 +11,6 @@ use App\Contracts\Factories\LawFactoryInterface;
 use App\Contracts\Factories\MatterFactoryInterface;
 use App\Contracts\Factories\MatterRelationFactoryInterface;
 use App\Contracts\Factories\MatterRelationSchemaFactoryInterface;
-use App\Contracts\Factories\PancakeFactoryInterface;
-use App\Contracts\Factories\PancakeStackFactoryInterface;
 use App\Contracts\Factories\RelationSchemaFactoryInterface;
 use App\Factories\AnnotationFactory;
 use App\Factories\ArticleFactory;
@@ -21,8 +19,6 @@ use App\Factories\LawFactory;
 use App\Factories\MatterFactory;
 use App\Factories\MatterRelationFactory;
 use App\Factories\MatterRelationSchemaFactory;
-use App\Factories\PancakeFactory;
-use App\Factories\PancakeStackFactory;
 use App\Factories\RelationSchemaFactory;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,8 +26,6 @@ class FactoryServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(PancakeFactoryInterface::class, PancakeFactory::class);
-        $this->app->singleton(PancakeStackFactoryInterface::class, PancakeStackFactory::class);
         $this->app->singleton(LawFactoryInterface::class, LawFactory::class);
         $this->app->singleton(MatterFactoryInterface::class, MatterFactory::class);
         $this->app->singleton(MatterRelationFactoryInterface::class, MatterRelationFactory::class);
